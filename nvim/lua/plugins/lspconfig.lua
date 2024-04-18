6af4f48941862eaabe79a773ae15a1effd57fd9d
Kevin Jay <kevinjay4567@gmail.com>
2024-04-18 06:12:06 -0500

    Update in Lsp and Mason configs

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -108,6 +108,11 @@ return {
 			on_attach = on_attach,
 		})
 
+		lspconfig["taplo"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
 		-- configure lua server (with special settings)
 		lspconfig["lua_ls"].setup({
 			capabilities = capabilities,
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,30 +1,23 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "jayp0521/mason-null-ls.nvim",
-  },
-  config = function()
-    -- import mason plugin safely
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"jayp0521/mason-null-ls.nvim",
+	},
+	config = function()
+		-- import mason plugin safely
+		local mason = require("mason")
 
-    -- import mason-lspconfig plugin safely
-    local mason_lspconfig = require("mason-lspconfig")
-
-    -- enable mason
-    mason.setup()
-
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "lua_ls",
-				"intelephense",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true,
-    })
-  end,
+		-- enable mason
+		mason.setup({
+			ensure_installed = {
+				"lua-language-server",
+				"stylua",
+				"prettier",
+				"tsserver",
+				"html",
+				"cssls",
+			}
+		})
+	end,
 }
